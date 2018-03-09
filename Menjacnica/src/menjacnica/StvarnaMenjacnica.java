1c27cd94b5fec49412681b491b84a2878cc81692
NikolaStojicic <nikolastojicicsd@gmail.com>
2018-03-09 19:27:17 +0100

    Uvedena klasa StvarnaMenjacnica koja implementira interfejs Menjacnica

---
+++ Menjacnica/src/menjacnica/StvarnaMenjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import valute.Valuta;
+
+public class StvarnaMenjacnica implements menjacnica.interfejs.Menjacnica {
+
+	@Override
+	public boolean dodajKurs(Valuta val, int dan) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean izbrisiKurs(Valuta val, int dan) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Valuta pronadjiValutu(int dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
