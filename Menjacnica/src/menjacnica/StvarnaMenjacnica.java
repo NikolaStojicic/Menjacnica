b84a9a0500e74046b1b742d4bf37e470773d94dd
NikolaStojicic <nikolastojicicsd@gmail.com>
2018-03-09 19:38:53 +0100

    Implementirane metode interfejsa u okviru klase StvarnaMenjacnica

--- Menjacnica/src/menjacnica/StvarnaMenjacnica.java
+++ Menjacnica/src/menjacnica/StvarnaMenjacnica.java
@@ -4,22 +4,28 @@ import valute.Valuta;
 
 public class StvarnaMenjacnica implements menjacnica.interfejs.Menjacnica {
 
+	private Valuta[] nizValuta;
+	public StvarnaMenjacnica(int kapacitet) {
+			nizValuta = new Valuta[31];
+	}
 	@Override
 	public boolean dodajKurs(Valuta val, int dan) {
-		// TODO Auto-generated method stub
-		return false;
+		if(dan < 0 || dan > 30) return false;
+		nizValuta[dan] = val;
+		return true;
 	}
 
 	@Override
 	public boolean izbrisiKurs(Valuta val, int dan) {
-		// TODO Auto-generated method stub
-		return false;
+		if(dan < 0 || dan > 30) return false;
+		nizValuta[dan] = null;
+		return true;
 	}
 
 	@Override
 	public Valuta pronadjiValutu(int dan) {
-		// TODO Auto-generated method stub
-		return null;
+		if(dan < 0 || dan > 30) return null;
+		return nizValuta[dan];
 	}
 
 }
