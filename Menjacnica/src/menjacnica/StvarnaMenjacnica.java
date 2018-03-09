5ea435e5a1f29b10ed8dd4b95f5bf51e8314d04e
NikolaStojicic <nikolastojicicsd@gmail.com>
2018-03-09 19:45:31 +0100

    Master - implementacija interfejsa

--- Menjacnica/src/menjacnica/StvarnaMenjacnica.java
+++ Menjacnica/src/menjacnica/StvarnaMenjacnica.java
@@ -1,24 +1,31 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
 import valute.Valuta;
 
 public class StvarnaMenjacnica implements menjacnica.interfejs.Menjacnica {
 
+	LinkedList<Valuta> listaValuta = new LinkedList<Valuta>();
+
 	@Override
 	public boolean dodajKurs(Valuta val, int dan) {
-		// TODO Auto-generated method stub
-		return false;
+		listaValuta.add(dan, val);
+		return true;
 	}
 
 	@Override
 	public boolean izbrisiKurs(Valuta val, int dan) {
-		// TODO Auto-generated method stub
-		return false;
+		listaValuta.remove(val);
+		return true;
 	}
 
 	@Override
 	public Valuta pronadjiValutu(int dan) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < listaValuta.size(); i++)
+			if (listaValuta.get(i).getDatumObjaveKursa().get(GregorianCalendar.DAY_OF_MONTH) == dan)
+				return listaValuta.get(i);
 		return null;
 	}
 
